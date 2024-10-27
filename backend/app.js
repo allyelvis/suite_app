be2180bf2e0599214882e209c456e27e552c2d75
Ally Elvis Nzeyimana <naelvis6569@gmail.com>
2024-10-27 00:15:34 +0000

    initial

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+// Port Configuration
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
